f4d78c35ebfc684b363c9e67d5808f510de7698e
Zaqueu Alves <zaqueu.za15@gmail.com>
2025-04-11 21:34:48 -0300

    update

--- src/app/todo/components/task-list/task-list.component.ts
+++ src/app/todo/components/task-list/task-list.component.ts
@@ -1,7 +1,13 @@
-import {Component, Input} from '@angular/core';
-import {MatCard, MatCardActions, MatCardTitle} from '@angular/material/card';
+import {Component, Input, OnInit} from '@angular/core';
+import {MatCard, MatCardActions, MatCardFooter, MatCardTitle} from '@angular/material/card';
 import {Task} from '../../model/task';
 import {MatButton} from '@angular/material/button';
+import {MatChip, MatChipSet} from '@angular/material/chips';
+import {NgClass} from '@angular/common';
+
+interface Itask extends Task {
+  statusClass: string;
+}
 
 @Component({
   selector: 'app-task-list',
@@ -10,13 +16,45 @@ import {MatButton} from '@angular/material/button';
     MatCard,
     MatCardTitle,
     MatCardActions,
-    MatButton
+    MatButton,
+    MatCardFooter,
+    MatChipSet,
+    MatChip,
+    NgClass
   ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
 
   @Input() taskList: Task[] = [];
 
+  tasks: Itask[] = [];
+
+
+  getStatusClass(status: string): string {
+    switch (status.toLowerCase()) {
+      case 'concluído':
+        console.log(status);
+        return 'status-concluido';
+      case "pendente":
+        return 'status-pendente';
+      case 'em andamento':
+        console.log(status);
+        return 'status-em-andamento';
+      default:
+        return '';
+    }
+  }
+
+  ngOnInit(): void {
+    this.tasks = this.taskList.map(value => ({
+      ...value,
+      statusClass: this.getStatusClass(value.status),
+    }));
+
+    console.log(this.tasks);
+  }
+
 }
+
